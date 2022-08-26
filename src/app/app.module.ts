8e4609781b3c70132e445f657a3ab9a21f81c2f2
Ioannis Boutsikas <8278212+iboutsikas@users.noreply.github.com>
2022-08-26 12:02:18 -0400

    Refactor title bar to its own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,12 +15,13 @@ import { HomeModule } from './home/home.module';
 import { DetailModule } from './detail/detail.module';
 
 import { AppComponent } from './app.component';
+import { TitleBarComponent } from './title-bar/title-bar.component';
 
 // AoT requires an exported function for factories
 const httpLoaderFactory = (http: HttpClient): TranslateHttpLoader =>  new TranslateHttpLoader(http, './assets/i18n/', '.json');
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TitleBarComponent, MenuBarComponent],
   imports: [
     BrowserModule,
     FormsModule,
