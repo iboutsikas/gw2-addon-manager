25ab5350429537f23d27e222aff5285be76794ce
Ioannis Boutsikas <8278212+iboutsikas@users.noreply.github.com>
2022-08-26 12:02:36 -0400

    Add menu-bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { DetailModule } from './detail/detail.module';
 
 import { AppComponent } from './app.component';
 import { TitleBarComponent } from './title-bar/title-bar.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
 
 // AoT requires an exported function for factories
 const httpLoaderFactory = (http: HttpClient): TranslateHttpLoader =>  new TranslateHttpLoader(http, './assets/i18n/', '.json');
