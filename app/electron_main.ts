e7e0ef1f873b33516268fe4553e1feb29ba7212b
Ioannis Boutsikas <8278212+iboutsikas@users.noreply.github.com>
2022-10-06 15:02:34 -0400

    Version bump

--- app/electron_main.ts
+++ app/electron_main.ts
@@ -2,7 +2,7 @@ import { app, BrowserWindow, shell, ipcMain, dialog } from 'electron';
 import * as path from 'path';
 import * as fs from 'fs';
 import * as storage from 'electron-json-storage';
-import installExtension, { REDUX_DEVTOOLS } from 'electron-devtools-installer';
+// import installExtension, { REDUX_DEVTOOLS } from 'electron-devtools-installer';
 
 import * as log from 'electron-log';
 
@@ -145,12 +145,12 @@ try {
   });
 
   app.whenReady().then(() => {
-    if (serve) 
-    {
-      installExtension(REDUX_DEVTOOLS)
-      .then((name) => console.log(`Added Extension:  ${name}`))
-      .catch((err) => console.log('An error occurred: ', err));
-    }    
+    // if (serve) 
+    // {
+    //   installExtension(REDUX_DEVTOOLS)
+    //   .then((name) => console.log(`Added Extension:  ${name}`))
+    //   .catch((err) => console.log('An error occurred: ', err));
+    // }    
     // win.webContents.openDevTools();
   });
 
